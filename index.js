ab5154ba41a5d07631fd7a26badf67074bd3083b
AhmadSayess <ahmadsayess1998@gmail.com>
2021-12-29 12:52:43 +0200

    step 2

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/test', (req, res) => {
+  res.send({status:200, message:"ok"}
+  )
+})
+
+
+
+let today = new Date();
+let h = today.getHours();
+let m = today.getMinutes();
+app.get('/time', (req, res) => {
+    res.send({status:200, message:`${h}:${m}`}
+    )
+  })
+
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+})
