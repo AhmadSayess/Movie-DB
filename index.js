d0e4a1f965d7b2005ee8ff9d3731a19652767aea
AhmadSayess <ahmadsayess1998@gmail.com>
2021-12-29 13:52:15 +0200

    step 4

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/test', (req, res) => {
   )
 })
 
+
 let today = new Date();
 let h = today.getHours();
 let m = today.getMinutes();
@@ -14,6 +15,25 @@ app.get('/time', (req, res) => {
     res.send({status:200, message:`${h}:${m}`}
     )
   })
+
+
+app.get('/hello/:id', (req, res) => {
+  let id = req.params.id
+    res.send({status:200, message:"hello", id}
+    )
+  })
+
+
+  app.get('/search', (req, res) => {
+    let s = req.query.s
+
+    if(s){
+      res.send({status:200, message:"ok", s}
+      )}
+      else {
+        res.send({status:500, error:true, message:"you have to provide a search"})
+      }
+    })
 
 
 app.listen(port, () => {
